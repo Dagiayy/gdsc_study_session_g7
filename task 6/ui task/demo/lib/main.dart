1ae8848d14fff94e32b5d5b0964a7cf61adc6b1c
Dagiayy <ayenewdagmawi@gmail.com>
2023-12-30 00:37:46 -0800

    done

---
+++ task 6/ui task/demo/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Home Page'), // Set your app bar title
+      ),
+      body: Center(
+        child: Text('Welcome to My Home Page!'), // Add your content here
+      ),
+    );
+  }
+}
